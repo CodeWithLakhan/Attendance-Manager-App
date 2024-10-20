907a5c4ea2e0959c53a429866977d295e048efc8
CodeWithLakhan <lakhandagrawal@gmail.com>
2024-10-21 00:09:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/attendencemaganer/AddSubject.java
@@ -0,0 +1,45 @@
+package com.example.attendencemaganer;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddSubject extends AppCompatActivity {
+
+    EditText subjectName, totalHours;
+    Button add;
+
+    DBHelper dbhelper;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_subject);
+
+        subjectName = findViewById(R.id.subjectName);
+        totalHours = findViewById(R.id.totalHours);
+        add = findViewById(R.id.add);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void addSubject(View view) {
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/attendencemaganer/DBHelper.java
@@ -0,0 +1,27 @@
+package com.example.attendencemaganer;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "DATABASE";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public DBHelper(@Nullable Context applicationContext) {
+        super(applicationContext, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
